ce7946e2e351fc9a025da230985ae033e1a96e9e
rstp <cruzenepro@yandex.ru>
2021-03-28 20:53:43 +0300

    add showing simple animations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,18 @@
 import { Component } from '@angular/core';
+import { transition,trigger,animate,style } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'], 
+  animations: [trigger ("objCreate" , 
+  [
+    transition(':enter', [
+      style({ opacity: 0, transform: 'translateY(15px)'}),
+      animate('1000ms', style({ opacity: 1,transform: 'translateY(-15px)' }))
+    ])
+  ])
+] 
 })
 export class AppComponent {
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { HeaderComponent } from './header/header.component';
 import { SliderComponent } from './slider/slider.component'; 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FooterComponent } from './footer/footer.component';
+import { SinginComponent } from './singin/singin.component';
 
 @NgModule({
   declarations: [
@@ -17,7 +18,8 @@ import { FooterComponent } from './footer/footer.component';
     CardComponent,
     HeaderComponent,
     SliderComponent,
-    FooterComponent
+    FooterComponent,
+    SinginComponent
   ],
   imports: [
     BrowserModule,
